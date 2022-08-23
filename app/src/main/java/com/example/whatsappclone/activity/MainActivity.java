1969fa68c03e5067ca245ac5169e072877a53de1
iamjaypierre <jepiierre@gmail.com>
2022-08-23 14:52:01 -0300

    Toolbar & Main Menu created

--- app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/com/example/whatsappclone/activity/MainActivity.java
@@ -1,16 +1,65 @@
 package com.example.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.whatsappclone.R;
+import com.example.whatsappclone.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuthentication();
+        mAuth = FirebaseAuth.getInstance();
+
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuSair:
+                deslogarUsuario();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario() {
+
+        try{
+            FirebaseAuth.getInstance().signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
